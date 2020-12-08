bb8a40548e7bc19ac5eb18749085076f9ca80a19
Zeeshan <zfaizal@hotmail.com>
2020-12-07 20:06:37 -0600

    Signed-off-by: Zeeshan <zfaizal@hotmail.com>

--- index.js
+++ index.js
@@ -46,9 +46,9 @@
     // const resized = tf.cast(smallImg, tf.float16)
     // const tf4d = tf.tensor4d(Array.from(resized.dataSync()), [1, 300, 300, 3]) // 600, 450
     const predictions = await model.executeAsync({ image_tensor: tf4d }, ['detection_boxes', 'num_detections', 'detection_classes', 'detection_scores'])
-    const num_detections += predictions[1].dataSync();
+    const num_detections += predictions[1].dataSync()
 
-    count.fillText("Number of shoes" + num_detections)
+    count.fillText("Number of shoes" + num_detections);
 
     tf4d.dispose();
     smallImg.dispose();
